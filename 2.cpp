ed04d29266ab297f8c690b9a541c7124c1a5ddc5
nguyenphan0101 <79673831+nguyenphan0101@users.noreply.github.com>
2021-02-27 16:31:12 +0700

    Add files via upload

---
+++ 2.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+int main()
+{ 
+ int x,i,j;
+ cout << "Nhap so dong: ";
+ cin >> x ; 
+   for ( i=1;i<=x;i++) 
+   { for( j=1;j<=i;j++) { 
+      cout << "*";
+ }
+     cout << endl;
+ }  
+}
